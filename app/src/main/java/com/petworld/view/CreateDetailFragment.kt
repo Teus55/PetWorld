4bf32784f7562f2a73dfd3d620585bd34fbbbafa
Teus55 <allanmateus55@gmail.com>
2024-06-30 10:28:49 +0700

    db

--- app/src/main/java/com/petworld/view/CreateDetailFragment.kt
+++ app/src/main/java/com/petworld/view/CreateDetailFragment.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.petworld.R
 import com.petworld.databinding.FragmentCreateBinding
 import com.petworld.databinding.FragmentCreateDetailBinding
+import com.petworld.viewmodel.DetailViewModel
+import com.petworld.viewmodel.ListViewModel
 
 class CreateDetailFragment : Fragment() {
     private lateinit var binding: FragmentCreateDetailBinding
+    private lateinit var viewModel:DetailViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,7 +23,7 @@ class CreateDetailFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentCreateDetailBinding.inflate(inflater,container,false)
         return binding.root
-
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
     }
 
 }
