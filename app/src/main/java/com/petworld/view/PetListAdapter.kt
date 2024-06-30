1d59b0ba8332d9fba612d2dcf7463d1cf62bb236
Renky32 <s160420066@student.ubaya.ac.id>
2024-06-30 19:26:00 +0700

    DataBindingAdapter

--- app/src/main/java/com/petworld/view/PetListAdapter.kt
+++ app/src/main/java/com/petworld/view/PetListAdapter.kt
@@ -1,18 +1,23 @@
 package com.petworld.view
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
+import com.petworld.R
+import com.petworld.databinding.FragmentHomeBinding
 import com.petworld.databinding.PetListItemBinding
 import com.petworld.model.PetWorld
 import com.squareup.picasso.Picasso
 
 class PetListAdapter(val petList:ArrayList<PetWorld>):RecyclerView.Adapter<PetListAdapter.PetViewHolder>() {
-    class PetViewHolder(var binding: PetListItemBinding):RecyclerView.ViewHolder(binding.root)
+    class PetViewHolder(var binding: PetListItemBinding):
+        RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PetViewHolder {
-        val binding = PetListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = PetListItemBinding.inflate(inflater,parent,false)
         return PetViewHolder(binding)
     }
 
@@ -20,10 +25,29 @@ class PetListAdapter(val petList:ArrayList<PetWorld>):RecyclerView.Adapter<PetLi
         return petList.size
     }
 
+
     override fun onBindViewHolder(holder: PetViewHolder, position: Int) {
-        holder.binding.txtTitle.text = petList[position].title
-        holder.binding.txtUser.text = "@"+petList[position].user
-        holder.binding.txtShortPara.text = petList[position].shortPara
+        val petWorld = petList[position]
+        holder.binding.petWorld = petWorld
+
+            holder.binding.clickListener = View.OnClickListener{
+                val id = petWorld.id.toString()
+                val imgUrl = petWorld.url.toString()
+                val user = "@${petWorld.user}"
+                val action = HomeFragmentDirections.actionDetailFragment(id,imgUrl,user)
+                Navigation.findNavController(it).navigate(action)
+            }
+        Picasso.get()
+            .load(petWorld.url)
+            .placeholder(R.drawable.ic_launcher_foreground)
+            .into(holder.binding.imgBerita)
+
+        holder.binding.executePendingBindings()
+
+
+        /*
+        holder.binding.txtUser.text = "@"+this
+        holder.binding.txtShortPara.text = this
         holder.binding.txtCategoryHewan.text = petList[position].catergory
         var url = petList[position].url
         val builder = Picasso.Builder(holder.itemView.context)
@@ -36,16 +60,11 @@ class PetListAdapter(val petList:ArrayList<PetWorld>):RecyclerView.Adapter<PetLi
             val user = "@"+petList[position].user.toString()
             val action = HomeFragmentDirections.actionDetailFragment(idBerita, imgURL, user)
             Navigation.findNavController(it).navigate(action)
-
-        }
-
+        }*/
     }
-
-        fun updatePetList(newPetList: List<PetWorld>) {
+    fun updatePetList(newPetList: List<PetWorld>) {
         petList.clear()
         petList.addAll(newPetList)
         notifyDataSetChanged()
     }
-
-
 }
